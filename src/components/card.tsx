47d21ad23447d37e748607124dc3923ec02303b1
CreatorXperience <allyearmustobey@gmail.com>
2023-07-26 23:10:53 +0100

    ft- refactor Home page

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -24,14 +24,14 @@ const Card = ({ Datas, index }: Tprops) => {
       )}
 
       {Datas ? (
-        <div className="w-[90%] rounded-2xl flex bg-[#F7F3F0] mx-4  mt-4 py-4 px-4">
+        <div className="w-[90%] rounded-2xl flex h-[140px] bg-[#F7F3F0] mx-4  mt-4 py-2 px-4">
           <div className="w-[90%]">
-            <div className="text-2xl text-gray-700">{Datas?.title}</div>
+            <div className="text-xl text-gray-700">{Datas?.title}</div>
 
             <div className="w-[80%] font-semibold text-sm">{Datas?.desc}</div>
 
             <Link to={`/play/${Datas.cat}/${Datas.id}`}>
-              <button className="w-[50%] flex justify-center  bg-[#253334] text-white py-2 mt-4 my-2 rounded-md">
+              <button className="w-[50%] flex justify-center  bg-[#253334] text-white py-2 mt-2  my-2 rounded-md">
                 Play Now <div className="mt-2 ml-2"> {svg.playIcon()}</div>
               </button>
             </Link>
