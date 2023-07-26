1e0eef21adf879be4023ee91dec9ed374a13ec8f
CreatorXperience <allyearmustobey@gmail.com>
2023-07-27 00:07:39 +0100

    ft- refactor several component

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -2,6 +2,8 @@ import React, { createRef } from "react";
 import { Link } from "react-router-dom";
 import svg from "../svgs/svg";
 import "../style.css";
+// @ts-ignore
+import logo from "../images/meditate.png";
 export default class Top extends React.Component {
   ref;
   constructor(props: any) {
@@ -35,17 +37,17 @@ export default class Top extends React.Component {
   render() {
     return (
       <div>
-        <div className="MainBody w-[100%]    absolute  z-10 text-white">
-          <div className="container  w-[100%]  flex justify-around mt-10 pl-2">
-            <div onClick={(e) => this.openNav(e)} className="hi">
-              {" "}
-              {svg.navIcon()}{" "}
-            </div>
-            <div className="translate-y-[-30%]">{svg.logo()}</div>
-            <div className="container rounded-[50%] w-[40px] translate-x-[-60%] h-[40px] bg-white">
+        <div className="MainBody w-[100%] absolute z-10 text-white pl-5">
+          <div className="w-[95%] flex justify-around">
+            <div className="container  w-[100%] flex mt-10 ">
+              <div onClick={(e) => this.openNav(e)}> {svg.navIcon()} </div>
+              <div className="translate-y-[-30%]  flex justify-center">
+                <img src={logo} className="w-[20%] " />
+              </div>
+
               <img
                 src="https://i.pinimg.com/564x/8a/f0/a2/8af0a298a18b75c6e9920d457d6a67fe.jpg"
-                className="rounded-[50%]"
+                className="rounded-[50%] w-[50px] h-[50px] "
                 alt="medic"
               />
             </div>
